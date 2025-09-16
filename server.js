8bb566998f5826bf35c3779b381c9a6da7992617
ViniciusOdorizziHoppe <viniciusodorizzi25@gmail.com>
2025-09-16 10:05:46 -0300

    adição da parte da cãmera

--- server.js
+++ server.js
@@ -5,24 +5,27 @@ import { fileURLToPath } from "url";
 
 const app = express()
 const porta = 2409;
-
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-app.use(express.static(path.join(__dirname, "html")))
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: "10mb"}))
 
 app.get('/', (req, res) => {
     res.send("<h2>Servidor Rodando BONITO!</h2>") 
 })
 
 app.get("/cadastro", (req, res) => {
-  res.sendFile(path.join(__dirname, "html", "cadastro.html"));
+  res.sendFile(path.join(__dirname, "public", "cadastro.html"));
 });
 app.post('/cadastro', (req,res) => {
-    res.sendFile(path.join(__dirname, "html", "cadastro.html"))
-    const {email , nome} = req.body
-    console.log(`Email: ${email} Nome: ${nome}`)
+    res.sendFile(path.join(__dirname, "public", "cadastro.html"))
+    const {email , senha} = req.body
+    console.log(`Email: ${email} Senha: ${senha}`)
+})
+app.get("/camera", (req,res) => {
+    res.sendFile(path.join(__dirname, "public", "camera.html"))
 })
 
 app.listen(porta, () => {
