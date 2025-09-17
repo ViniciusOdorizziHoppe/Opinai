e9733399c75602813af6dd0c84a38e545faea5e6
Vinicius Odorizzi Hoppe <viniciusodorizzi25@gmail.com>
2025-09-17 08:35:23 -0300

    feito a parte de salvar as fotos dentro de uma pasta

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 import express from "express"
 import path from "path"
 import mysql2 from "mysql2"
-import { fileURLToPath } from "url";
+import { fileURLToPath } from "url"
+import fs from "fs"
 
 const app = express()
 const porta = 2409;
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
+const uploadDir = path.join(__dirname, "uploads")
+if (!fs.existsSync(uploadDir)) {
+fs.mkdirSync(uploadDir, { recursive: true });
+}
 app.use(express.static(path.join(__dirname, "public")))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({limit: "10mb"}))
@@ -28,6 +33,27 @@ app.get("/camera", (req,res) => {
     res.sendFile(path.join(__dirname, "public", "camera.html"))
 })
 
+
+app.post("/uploads", (req, res) => {
+  const { image } = req.body;
+  if (!image) {
+    return res.status(400).send("Nenhuma imagem recebida");
+  }
+  const base64Data = image.replace(/^data:image\/png;base64,/, "");
+  const filePath = path.join(uploadDir, `foto_${Date.now()}.png`);
+
+  fs.writeFile(filePath, base64Data, "base64", (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Erro ao salvar a imagem");
+    }
+    res.send("Imagem salva em: " + filePath);
+  });
+});
+
+
+
 app.listen(porta, () => {
     console.log(`Servidor Rodando na Porta: LocalHost: ${porta}`)
 })
+
